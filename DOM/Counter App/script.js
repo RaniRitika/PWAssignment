2a3fd607ac1b43b2c1bb1f446488eee427013892
Ritika Rani <123168186+RaniRitika@users.noreply.github.com>
2023-07-23 10:42:07 +0530

    Add files via upload

---
+++ DOM/Counter App/script.js
@@ -0,0 +1,19 @@
+function inc(){
+let tag= document.getElementById("zero")
+let value=tag.value;
+value=value+1
+tag.value=value;
+}
+
+function dec(){
+    let tag= document.getElementById("zero")
+    let value=tag.value;
+    value=value-1
+    tag.value=value;
+    }
+function res(){
+    let tag= document.getElementById("zero")
+    let value=tag.value;
+    value=0
+    tag.value=value;
+}
